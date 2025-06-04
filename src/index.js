08323a064c7139f9318eae41d740a338d1ff0de1
Ingamha <ingamha@gmail.com>
2025-06-04 17:51:25 +0200

    Added AI-api integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: [response.data.answer],
+    autoStart: true,
+  }).start();
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "How to make pizza doug: Mix flour, salt and a little yeast.",
-    delay: 10,
-    autoStart: true,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "1a6546fbd4afe78fc90o08t2850b2e3e";
+  let context =
+    "You are a cooking expert, and make easy recipes. You will generate a short recipe in basic HTML based on the user instructions below. Only give ingredients and instructions. Provide measurements in the metric system.";
+  let prompt = `User instructions: Generate a recipe based ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-form-generator");
