e1266e44f5627abcc3ed76ee59ef6ac60785f7bb
Ingamha <ingamha@gmail.com>
2025-06-05 13:32:41 +0200

    Improved user experience, animation in css/js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: [response.data.answer],
+    strings: response.data.answer,
     autoStart: true,
-  }).start();
+    delay: 1,
+    cursor: "",
+  });
 }
 
 function generateRecipe(event) {
@@ -12,9 +14,13 @@ function generateRecipe(event) {
 
   let apiKey = "1a6546fbd4afe78fc90o08t2850b2e3e";
   let context =
-    "You are a cooking expert, and make easy recipes. You will generate a short recipe in basic HTML based on the user instructions below. Only give ingredients and instructions. Provide measurements in the metric system.";
+    "You are a cooking expert, and make easy recipes. You will generate a short recipe in HTML based on the user instructions below. Only provide ingredients and instructions with measurements in the metric system.";
   let prompt = `User instructions: Generate a recipe based ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class= "generating"> Generating recipe for ${instructionsInput.value}... </div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
